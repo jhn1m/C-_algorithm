e036a41b5271a0ad78b59dff00e4228eee68a588
JAEHYEON LEE <58750412+jhn1m@users.noreply.github.com>
2021-09-13 21:33:49 +0900

    Create ex3.cs
    To print a cat's appearance
    
    Using System.Console.WriteLine and how to print slash

---
+++ ex3.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Baekjoon 
+{
+    class Program
+    {
+        static void Main() 
+        {
+            Console.WriteLine("\\    /\\");
+            Console.WriteLine(" )  ( ')");
+            Console.WriteLine("(  /  )");
+            Console.WriteLine(" \\(__)|");
+        }
+    }
+}
