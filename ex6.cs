05659333d7e5c2d1cd0256edd1900bb11584c6e9
JAEHYEON LEE <58750412+jhn1m@users.noreply.github.com>
2021-09-15 21:43:27 +0900

    Create ex6.cs
    Use ReadLine to output the difference between the values entered by the two integers.
    unlike Java, C# must be converted to int type after receiving String. Since the input values are in one line, a parse was used to separate them using an array.

---
+++ ex6.cs
@@ -0,0 +1,17 @@
+using System;
+using static System.Console;
+namespace Baekjoon
+{
+	public class main 
+	{
+		public static void Main() 
+		{
+			string input = Console.ReadLine();
+			string [] input_array = input.Split();
+			int a = int.Parse(input_array[0]);
+			int b = int.Parse(input_array[1]);
+			
+			WriteLine(a - b);
+		}
+	}
+}
