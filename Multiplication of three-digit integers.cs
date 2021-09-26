52962a7d45ed9db3a3a3b0df6314d6212b81fdce
JAEHYEON LEE <58750412+jhn1m@users.noreply.github.com>
2021-09-27 00:30:38 +0900

    Create Multiplication of three-digit integers.cs
    (three digits) × (three digits) is achieved through the following process:
    
                   472 ....(1)
                X 385 ....(2)
    ---------------
                2360  ....(3)
              3776    ....(4)
            1416      ....(5)
    ---------------
            181720  ....(6)
    
    When you are given a three-digit natural number for positions (1) and (2), write a program to find values for positions (3), (4), (5) and (6).

---
+++ Multiplication of three-digit integers.cs
@@ -0,0 +1,20 @@
+using System;
+using static System.Console;
+namespace Baekjoon
+{
+	public class main 
+	{
+		public static void Main() 
+		{
+			int a, b;
+			
+			a = int.Parse(Console.ReadLine());
+			b = int.Parse(Console.ReadLine());
+			
+			WriteLine(a * (b % 10) );
+			WriteLine(a * (b % 100 / 10) );
+			WriteLine(a * (b / 10 / 10) );
+			WriteLine(a * b);
+		}
+	}
+}
