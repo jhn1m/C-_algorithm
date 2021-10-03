1b252b4077213e15c7589cc776d651161909515d
JAEHYEON LEE <58750412+jhn1m@users.noreply.github.com>
2021-10-03 14:45:26 +0900

    Update Create Choosingthequadrant.cs

--- Create Choosingthequadrant.cs
+++ Create Choosingthequadrant.cs
@@ -1,1 +1,22 @@
+using System;
+using static System.Console;
+namespace Baekjoon
+{
+    public class main
+    {
+        public static void Main()
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
 
+            if (a > 0 && b > 0)
+                Console.WriteLine("1");
+            else if (a < 0 && b > 0)
+                Console.WriteLine("2");
+            else if (a < 0 && b < 0)
+                Console.WriteLine("3");
+            else
+                Console.WriteLine("4");
+        }
+    }
+}
